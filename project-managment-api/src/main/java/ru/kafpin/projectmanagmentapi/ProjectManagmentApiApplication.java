9c02ad4860effa46a3ae8a11e92527a1cd8ab874
vehxi <vehxi.dev@gmail.com>
2026-09-12 02:27:19 +0300

    feat: add hello endpoint

---
+++ project-managment-api/src/main/java/ru/kafpin/projectmanagmentapi/ProjectManagmentApiApplication.java
@@ -0,0 +1,13 @@
+package ru.kafpin.projectmanagmentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagmentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagmentApiApplication.class, args);
+    }
+
+}
---
+++ project-managment-api/src/test/java/ru/kafpin/projectmanagmentapi/ProjectManagmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kafpin.projectmanagmentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagmentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
